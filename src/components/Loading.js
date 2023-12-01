1545c65281395a9574f105e21435ea04fe446e09
kiranr7353 <62291859+kiranr7353@users.noreply.github.com>
2023-12-01 11:55:57 +0530

    first commit

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import { ActivityIndicator, Text, View } from 'react-native'
+import React from 'react'
+
+const Loading = (props) => {
+  return (
+    <View className="flex-1 flex justify-center items-center">
+      <ActivityIndicator {...props} />
+    </View>
+  )
+}
+
+export default Loading
